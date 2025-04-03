eb044f3a2e9b203c269b40b9d091547fd2249060
Manish Bhande <manish.bhande90@gmail.com>
2025-04-04 00:07:21 +0530

    First Commit.

---
+++ Sources/MBRedux/MBRedux.swift
@@ -0,0 +1,166 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+import Combine
+
+/// Protocol representing an action in the Redux flow.
+/// Any action that is dispatched must conform to this protocol.
+protocol ReduxAction {
+    /** Use to all action*/
+}
+
+/// Protocol that represents the state in the Redux flow.
+/// The state must conform to `Hashable` to enable comparisons based on hash values.
+protocol StateType: Hashable {
+    /** Must be adopted by state*/
+}
+
+/// Extend the `StateType` protocol to provide a custom equality operator (`==`).
+/// This compares two `StateType` instances by their `hashValue`.
+extension StateType {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.hashValue == rhs.hashValue
+    }
+}
+
+// Typealias that defines the `Reducer` type.
+// A `Reducer` takes a `ReduxAction` and a current state (`S?`) and returns an updated state (`S?`).
+typealias Reducer<S: StateType> = (ReduxAction, S?) -> S?
+
+// Private struct to encapsulate the reducer logic.
+// This struct holds the reducer function and provides an `apply` method to apply actions to the state.
+private struct ReduxReducer<S: StateType> {
+    // The reducer function
+    let reducer: Reducer<S>
+    
+    // Initializer to set the reducer function
+    init(reducer: @escaping Reducer<S>) {
+        self.reducer = reducer
+    }
+    
+    // Applies the given action to the current state, returning the updated state
+    func apply(action: ReduxAction, toState state: S?) -> S? {
+        return reducer(action, state)
+    }
+}
+
+/// Private struct to manage state updates and subscriptions.
+/// This struct allows subscribers to listen for changes in state before and after the update.
+private struct ReduxSubscription<S: StateType> {
+    
+    // Publishers to track the state before and after an update
+    private let beforeSateUpdate = PassthroughSubject<S?, Never>()
+    private let afterStateUpdate = PassthroughSubject<S?, Never>()
+    
+    /// Sends the state before it is updated
+    func willUpdateState(_ state: S?) {
+        beforeSateUpdate.send(state)
+    }
+    
+    /// Sends the state after it is updated
+    func didUpdateState(_ state: S?) {
+        afterStateUpdate.send(state)
+    }
+    
+    /// Combines `beforeStateUpdate` and `afterStateUpdate` to emit the new state only when it changes.
+    /// This is useful for notifying subscribers only when the state actually changes.
+    func subscribe() -> AnyPublisher<S?, Never> {
+        beforeSateUpdate
+            .combineLatest(afterStateUpdate)
+            .filter {
+                // Only emit when the state has changed (checked by hashValue)
+                $0.hashValue != $1.hashValue
+            }.map { _, newState in
+                newState
+            }.eraseToAnyPublisher() // Returns a publisher
+    }
+    
+    /// Subscribe to a specific path within the state (using KeyPath) and emit only the changed part of the state.
+    func subscribe<P: Hashable>(path: KeyPath<S, P>) -> AnyPublisher<P, Never> {
+        beforeSateUpdate.map {
+            // Access the state at the specific path before the update
+            $0?[keyPath: path]
+        }.combineLatest(
+            afterStateUpdate.map {
+                // Access the state at the specific path after the update
+                $0?[keyPath: path]
+            }
+        ).filter {
+            // Only emit when the value at the specific path has changed
+            $0.hashValue != $1.hashValue
+        }
+        .compactMap { _, newValue in
+            newValue // Return the updated value
+        }.eraseToAnyPublisher() // Return a publisher for the specific value
+    }
+}
+
+/// ReduxStore class encapsulates the entire Redux flow for managing the state.
+/// This class includes functionality to register reducers, dispatch actions, and manage state updates.
+class ReduxStore<S: StateType> {
+    
+    // The current state of the store, which can be nil initially.
+    private var state: S?
+    
+    // The reducer to manage state changes, initialized when registered.
+    private var reducer: ReduxReducer<S>?
+    
+    // Subscription manager to handle state change notifications.
+    private let subscription: ReduxSubscription<S> = .init()
+    
+    // A dedicated queue to synchronize state changes and actions.
+    private let reduxQueue = DispatchQueue(label: "com.reduxStore.queue")
+    
+    /// Initializes an empty store with no state.
+    init () {
+        self.state = nil
+    }
+    
+    /// Registers a reducer function, but ensures that it can only be registered once.
+    /// Throws an error if a reducer is already registered.
+    func register(reducer: @escaping Reducer<S>) throws {
+        guard self.reducer == nil else {
+            // Throw error if a reducer is already registered.
+            throw NSError(domain: "ReduxError.reducerAlreadyRegistered", code: -1)
+        }
+        // Initialize the ReduxReducer with the provided reducer function.
+        self.reducer = .init(reducer: reducer)
+    }
+    
+    /// Dispatches an action to update the state.
+    /// The state is updated inside a sync block to ensure thread safety.
+    func dispatch(_ action: ReduxAction) {
+        reduxQueue.sync { [weak self] in
+            guard let self else {
+                return
+            }
+            // Notify subscribers that the state will be updated.
+            subscription.willUpdateState(state)
+            // Apply the reducer to the current state and the action.
+            state = reducer?.apply(action: action, toState: state)
+            // Notify subscribers that the state has been updated.
+            subscription.didUpdateState(state)
+        }
+    }
+    
+    /// Returns a publisher that emits the entire state when it changes.
+    func subscribe() -> AnyPublisher<S?, Never> {
+        subscription.subscribe()
+    }
+    
+    /// Returns a publisher that emits a specific part of the state (based on the path) when it changes.
+    func subscribe<P: Hashable>(path: KeyPath<S, P>) -> AnyPublisher<P, Never> {
+        subscription.subscribe(path: path)
+    }
+    
+    /// Return value of State
+    func getState() -> S? {
+        state
+    }
+    
+    /// Return value at specifed path from state
+    func getState<P>(path: KeyPath<S, P>) -> P? {
+        getState()?[keyPath: path]
+    }
+}
---
+++ Tests/MBReduxTests/MBReduxTests.swift
@@ -0,0 +1,155 @@
+import XCTest
+import Combine
+@testable import MBRedux
+
+// Define a simple test state and action
+private struct TestState: StateType {
+    var value: Int
+}
+
+private enum TestAction: ReduxAction {
+    case increment
+    case decrement
+    case noChange
+}
+
+private enum MockReducer {
+    static func reduce(action: ReduxAction, state: TestState?) -> TestState? {
+        switch action {
+        case TestAction.increment:
+            return TestState(value: (state?.value ?? 0) + 1)
+        case TestAction.decrement:
+            return TestState(value: (state?.value ?? 0) - 1)
+        default:
+            return state
+        }
+    }
+}
+
+final class ReduxStoreTests: XCTestCase {
+    
+    // The Redux store instance to test
+    private var store: ReduxStore<TestState>!
+    
+    // The cancellables to hold subscriptions
+    var cancellables: Set<AnyCancellable> = []
+    
+    override func setUp() {
+        super.setUp()
+        // Initialize the store with an initial state
+        store = ReduxStore<TestState>()
+    }
+    
+    override func tearDown() {
+        // Reset the store and cancellables after each test
+        cancellables.removeAll()
+        store = nil
+        super.tearDown()
+    }
+    
+    func testReducerRegistration() {
+        // Try registering the reducer
+        XCTAssertNoThrow(try store.register(reducer: MockReducer.reduce))
+        // Try to register the same reducer again, should throw error
+        XCTAssertThrowsError(try store.register(reducer: MockReducer.reduce)) { error in
+            XCTAssertEqual((error as NSError).domain, "ReduxError.reducerAlreadyRegistered")
+        }
+    }
+    
+    func testDispatchAction() throws {
+        // Register the reducer
+        try store.register(reducer: MockReducer.reduce)
+        // Dispatch actions
+        store.dispatch(TestAction.increment)
+        store.dispatch(TestAction.increment)
+        // Check the state after dispatching
+        store.subscribe()
+            .sink { newState in
+                XCTAssertEqual(newState?.value, 2) // We dispatched increment twice
+            }
+            .store(in: &cancellables)
+        
+    }
+    
+    @MainActor
+    func testStateSubscription() throws {
+        // Register the reducer
+        try store.register(reducer: MockReducer.reduce)
+        // Expectation for state change subscription
+        let expectation = expectation(description: "State should be updated")
+        store.subscribe()
+            .sink { newState in
+                if newState?.value == 1 {
+                    expectation.fulfill() // Fulfill when state is updated to value 1
+                }
+            }
+            .store(in: &cancellables)
+        
+        // Dispatch increment action
+        store.dispatch(TestAction.increment)
+        
+        // Wait for the state change to be triggered
+        wait(for: [expectation], timeout: 0.5)
+    }
+    
+    @MainActor
+    func testStatePathSubscription() throws {
+        // Register the reducer
+        try store.register(reducer: MockReducer.reduce)
+        // Expectation for state path change subscription
+        let expectation = self.expectation(description: "State path value should be updated")
+        store.subscribe(path: \TestState.value)
+            .sink { newValue in
+                if newValue == 1 {
+                    expectation.fulfill() // Fulfill when value is updated to 1
+                }
+            }
+            .store(in: &cancellables)
+        // Dispatch increment action
+        store.dispatch(TestAction.increment)
+        // Wait for the state path change to be triggered
+        wait(for: [expectation], timeout: 0.5)
+    }
+    
+    // If this fails adjust timout
+    @MainActor
+    func testNoStateChange() throws {
+        // Register the reducer
+        try store.register(reducer: MockReducer.reduce)
+        // Expectation for state path change subscription
+        let expectation = self.expectation(description: "State change should not be call")
+        // Create default state
+        store.subscribe().sink { state in
+            XCTAssertNotNil(state)
+        }.store(in: &cancellables)
+        store.dispatch(TestAction.increment)
+        // Check for flag if subscriber calls
+        var isUpdateCalled = false
+        store.subscribe(path: \.value)
+            .sink { newState in
+                isUpdateCalled = true
+            }
+            .store(in: &cancellables)
+        // dispach no action
+        store.dispatch(TestAction.noChange)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            XCTAssertFalse(isUpdateCalled)
+            expectation.fulfill()
+        }
+        // Wait for both before and after state update expectations
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testState() throws {
+        XCTAssertNil(store.getState())
+        XCTAssertNil(store.getState(path: \.value))
+        // Register the reducer
+        try store.register(reducer: MockReducer.reduce)
+        // Expectation for state path change subscription
+        // Create default state
+        store.dispatch(TestAction.increment)
+        XCTAssertNotNil(store.getState())
+        XCTAssertEqual(store.getState(path: \.value), 1)
+    }
+    
+}
