315a106ad8dd9cd689710969761521b48ed24715
Manish Bhande <manish.bhande90@gmail.com>
2025-08-16 01:28:16 +0530

    Available to iOS 16 only.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MBRedux",
-    platforms: [.iOS(.v13), .macOS(.v10_15)],
+    platforms: [.iOS(.v16), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
